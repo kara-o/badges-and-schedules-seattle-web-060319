5e892936593ed38c39907f6966235847f30a515b
Kara O <karajo.odle@gmail.com>
2019-05-15 18:39:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,4 +2,10 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(array_attendees)
+def batch_badge_creator(array)
+  new_array = []
+  for i in array[0..array.length]
+    new_array << "Hello, my name is #{i}"
+  end
+  new_array
+end
